813040cedd83b832a90a6a80f44c2ab20841a99c
foglabs <foglaboratories@gmail.com>
2019-08-28 14:27:24 -0400

    'simplest possible backend'

--- src/App.js
+++ src/App.js
@@ -2,36 +2,49 @@ import React from 'react';
 // import logo from './logo.svg';
 import ReactDOM from 'react-dom';
 
-import {Editor, EditorState, RichUtils, convertToRaw} from 'draft-js';
+import {Editor, EditorState, RichUtils, convertToRaw, convertFromRaw} from 'draft-js';
 import {stateToHTML} from 'draft-js-export-html';
 
 import update from 'immutability-helper';
+import axios from 'axios';
+
+import videojs from 'video.js';
 
 import './App.css';
+
+let TITLE_BOXTYPE = 1;
+let TEXTBOX_BOXTYPE = 2;
 
 function createMarkup(html) {
   return {__html: html};
 }
 
-
 class App extends React.Component {
   constructor(props){
     super(props);
+
+    // in realité, this will take project_id and figure out which boxes it needs to loadBox, then throw them into state
     
+    // this.state = {
+    //   titles: {
+    //     "4": {id: 4, data: '', text: 'This is the title text'},
+    //   },
+    //   textboxes: {
+    //     "5": {id: 5, data: '', text: 'This is a text box. And so it goes on and on.'}
+    //   },
+    //   playlist_items: {
+    //     "1": {id: 1, title: 'title1', guid: 'cpb-aacip_37-97kps40n', in_time: 5, out_time: 8},
+    //     "2": {id: 2, title: 'title2', guid: 'cpb-aacip_37-20sqvcx0', in_time: 6, out_time: 9},
+    //     "3": {id: 3, title: 'title3', guid: 'cpb-aacip_507-gf0ms3kn9k', in_time: 7, out_time: 10},
+    //   },
+
+    //   editor_open: false
+    // };
+
     this.state = {
-      titles: {
-        "4": {id: 4, text: 'This is the title text'},
-      },
-      textboxes: {
-        "5": {id: 5, text: 'This is a text box. And so it goes on and on.'}
-      },
-      playlist_items: {
-        "1": {id: 1, title: 'title1', guid: 'cpb-aacip_37-97kps40n', in_time: 5, out_time: 8},
-        "2": {id: 2, title: 'title2', guid: 'cpb-aacip_37-20sqvcx0', in_time: 6, out_time: 9},
-        "3": {id: 3, title: 'title3', guid: 'cpb-aacip_507-gf0ms3kn9k', in_time: 7, out_time: 10},
-      },
-
-
+      titles: {},
+      textboxes: {},
+      playlist_items: {},
       editor_open: false
     };
 
@@ -40,6 +53,98 @@ class App extends React.Component {
     this.handleSubmit = this.handleSubmit.bind(this);
 
     this.showPreview = this.showPreview.bind(this);
+    this.saveEditorState = this.saveEditorState.bind(this);
+
+    this.loadProject = this.loadProject.bind(this);
+    this.loadBox = this.loadBox.bind(this);
+
+    // load whatever we got
+    // this.loadProject(1);
+  }
+
+  // shouldComponentUpdate(nextProps, nextState) {
+  //   console.log(this.state)
+  //   return true;
+  //   // if( this.state.titles !== nextState.titles || this.state.textboxes !== nextState.textboxes || this.state.playlist_items !== nextState.playlist_items) {
+  //   //   return true;
+  //   // } else {
+  //   //   return false;
+  //   // }
+  // }
+
+  newProject() {
+    console.log("hello")
+    // delete everythign and get new elements
+    axios.get('http://localhost:3001/init_project').then( (resp) => {
+      console.log('got that dang newproject ' + resp.status)
+      console.log(resp)
+
+      this.setState({titles: {}, textboxes: {}, playlist_items: {}});
+      this.loadProject(1);
+    });    
+  }
+
+  loadProject(id) {
+    axios.get('http://localhost:3001/project/'+id).then( (resp) => {
+      console.log('got that dang loadproject ' + resp.status)
+      console.log(resp)
+
+      let box_ids = resp.data;
+      console.log(box_ids)
+      if(!box_ids) box_ids = [];
+
+      // needs arrow to presever this behavior inside this block!
+      box_ids.forEach( (box_id) => {
+        this.loadBox(box_id);
+      });
+    });
+  }
+
+  saveProject() {
+    let titles = this.state.titles;
+    let textboxes = this.state.textboxes;
+    
+    // how u struckchur
+    // let playlist_items = this.state.playlist_items;
+
+    Object.keys(titles).forEach(function(title_id, index) {
+      this.saveBox(TITLE_BOXTYPE, title_id, titles[title_id].data, titles[title_id].text);
+    });
+
+    Object.keys(textboxes).forEach(function(textbox_id, index) {
+      this.saveBox(TEXTBOX_BOXTYPE, textbox_id, textboxes[textbox_id].data, textboxes[textbox_id].text);
+    });
+  }
+
+  loadBox(id) {
+    console.log(id+"i tried!")
+    axios.get('http://localhost:3001/boxes/'+id).then( (resp) => {
+      console.log('got that dang loadbox ' + resp.status)
+      console.log(resp)
+      let box = resp.data;
+      let type = box.box_type === TITLE_BOXTYPE ? 'titles' : 'textboxes';
+      this.setState({[type]: update(this.state[type],
+          {[id]: {$set: box } } )
+      });
+
+    });
+  }
+
+  saveBox(box_type, id, data, text) {
+    if(id){
+      
+      axios.patch('http://localhost:3001/boxes/'+id, {box_type: box_type, data: data, text: text}).then( (resp) => {
+        console.log('got that dang response ' + resp.status)
+        console.log(resp)
+      });
+
+    } else {
+      // this will only get called when initializing the project...
+      axios.post('http://localhost:3001/boxes', {box_type: box_type, data: data, text: text}).then( (resp) => {
+        console.log('got that dang response ' + resp.status)
+        console.log(resp)
+      });
+    }
   }
 
   // for handling state of text inputs!
@@ -96,7 +201,21 @@ class App extends React.Component {
     return classes;
   }
 
+  saveEditorState(id, json_data, box_type) {
+    console.log('Save Editor Staet: ')
+    console.log(id)
+    console.log(json_data)
+    this.setState({[box_type]: update(this.state[box_type],
+        {[id]: 
+          {data: {$set: json_data }
+        }
+      })
+    });
+  
+  }
+
   showPreview(id, html, box_type) {
+    console.log('Show Preview: ')
     console.log(id)
     console.log(html)
     this.setState({[box_type]: update(this.state[box_type],
@@ -110,10 +229,27 @@ class App extends React.Component {
 
   render() {
     // console.log(this.state.playlist_items)
+
+    let layout;
+    if(Object.keys(this.state.titles).length > 0 && Object.keys(this.state.textboxes).length > 0) {
+      layout = (
+        <Layout
+          title={ Object.entries(this.state.titles)[0][1] }
+          textbox={ Object.entries(this.state.textboxes)[0][1] }
+          playlist_items={ this.state.playlist_items }
+        />
+      );
+    }
+
     return (
       <div>
         <div id="menu-bar">
           <div className="spacer">
+            
+            <button id="new-project" onClick={ () => this.newProject() } className="menu-item">
+              New Project
+            </button>
+
             <button id="edit" onClick={ () => this.toggleEditor() } className="menu-item">
               Edit
             </button>
@@ -121,11 +257,6 @@ class App extends React.Component {
             <button className="menu-item">
               Save
             </button>
-
-            <button className="menu-item">
-              thing
-            </button>
-
           </div>
         </div>
 
@@ -142,6 +273,7 @@ class App extends React.Component {
                 playlist_items={ this.state.playlist_items }
 
                 showPreview={ this.showPreview }
+                saveEditorState={ this.saveEditorState }
               />
             </div>
           </div>
@@ -151,11 +283,7 @@ class App extends React.Component {
         
               <h2>Preview</h2>
 
-              <Layout
-                title={ this.state.titles["4"] }
-                textbox={ this.state.textboxes["5"] }
-                playlist_items={ this.state.playlist_items }
-              />
+              { layout }
             </div>
           </div>
         </div>
@@ -167,16 +295,27 @@ class App extends React.Component {
 class BoxEditor extends React.Component {
     constructor(props) {
     super(props);
-    this.state = {editorState: EditorState.createEmpty()};
+
+    let editor_data;
+    if (this.props.data) {
+      editor_data = EditorState.createWithContent(convertFromRaw(this.props.data))
+    } else {
+      editor_data = EditorState.createEmpty();
+    }
+
+    this.state = {editorState: editor_data};
     // this.onChange = (editorState) => this.setState({editorState});
     this.onChange = (editorState) => {
 
       this.setState({editorState}, () => {
-        // this fires after setstate has compelted
-        this.props.showPreview( this.props.object_id, this.getPreview(this.props.object_id), this.props.box_type );  
 
+        // this block fires after setstate has compelted
+        let contentState = this.state.editorState.getCurrentContent();
+        // grab current state of editor
+        this.props.saveEditorState( this.props.object_id, this.getEditorData(contentState), this.props.box_type );  
+        // grab current output of editor
+        this.props.showPreview( this.props.object_id, this.getPreview(contentState), this.props.box_type );  
         return true;
-
       });
       
     };
@@ -191,10 +330,12 @@ class BoxEditor extends React.Component {
     return 'not-handled';
   }
 
-  getPreview(id) {
-    let contentState = this.state.editorState.getCurrentContent();
-    let html = stateToHTML(contentState);
-    return html;
+  getPreview(content) {
+    return stateToHTML(content);;
+  }
+
+  getEditorData(content) {
+    return JSON.stringify(convertToRaw(content));
   }
 
   render() {
@@ -203,7 +344,6 @@ class BoxEditor extends React.Component {
         <Editor
           editorState={ this.state.editorState }
           onChange={ this.onChange }
-          // onKeyUp={ () => this.props.showPreview( this.getPreview(this.props.object_id) ) }
           handleKeyCommand={ this.handleKeyCommand }
         />
       </div>
@@ -239,6 +379,10 @@ class PageEditor extends React.Component {
     //   </div>
     // );
 
+    let title_data;
+    if(this.props.titles[id]) {
+      title_data = this.props.titles[id].data
+    }
 
     return (
       <div key={ id }>
@@ -250,8 +394,12 @@ class PageEditor extends React.Component {
           <div className="edit-box">
             <BoxEditor
               object_id={ id }
+              data={ title_data }
+
               box_type={ 'titles' }
+
               showPreview={ this.props.showPreview }
+              saveEditorState={ this.props.saveEditorState }
             />
           </div>
         </label>
@@ -278,6 +426,11 @@ class PageEditor extends React.Component {
    //    </div>
    //  ); 
 
+    let textbox_data;
+    if(this.props.textboxes[id]) {
+      textbox_data = this.props.textboxes[id].data
+    }
+
     return (
       <div key={ id }>
         <label>
@@ -288,8 +441,12 @@ class PageEditor extends React.Component {
           <div className="edit-box">
             <BoxEditor
               object_id={ id }
+              data={ textbox_data }
+
               box_type={ 'textboxes' }
+
               showPreview={ this.props.showPreview }
+              saveEditorState={ this.props.saveEditorState }
             />
           </div>
         </label>
@@ -400,24 +557,39 @@ class Layout extends React.Component {
   }
 
   render(){
-    return (
-      <div className="container">
-        <div className="half-pane">
-          <Title
-            id={ this.props.title.id }
-            text={ this.props.title.text }
-          />
-          <Textbox
-            id={ this.props.textbox.id }
-            text={ this.props.textbox.text }
-          />
+    const videoJsOptions = {
+      autoplay: false,
+      controls: true,
+      sources: [{
+        src: 'https://avalon-mediacache-test.s3.amazonaws.com/anamorphTEST.mp4',
+        type: 'video/mp4'
+      }]
+    }
+
+    let template = '';
+
+    if(this.props.title && this.props.title.id){
+      template = (
+        <div className="container">
+          <div className="half-pane">
+            <Title
+              id={ this.props.title.id }
+              text={ this.props.title.text }
+            />
+            <Textbox
+              id={ this.props.textbox.id }
+              text={ this.props.textbox.text }
+            />
+          </div>
+          
+          <div className="half-pane">
+            <VideoPlayer { ...videoJsOptions } />
+            <Playlist playlist_items={ this.props.playlist_items } />;
+          </div>
         </div>
-        
-        <div className="half-pane">
-          <Playlist playlist_items={ this.props.playlist_items } />;
-        </div>
-      </div>
-    );
+      );
+    }
+    return template;
   }
 
 }
@@ -476,5 +648,36 @@ class Playlist extends React.Component {
     );
   }
 }
+
+class VideoPlayer extends React.Component {
+  componentDidMount() {
+    // instantiate Video.js
+    this.player = videojs(this.videoNode, this.props, function onPlayerReady() {
+      console.log('onPlayerReady', this)
+    });
+  }
+
+  // destroy player on unmount
+  componentWillUnmount() {
+    if (this.player) {
+      this.player.dispose()
+    }
+  }
+
+  // wrap the player in a div with a `data-vjs-player` attribute
+  // so videojs won't create additional wrapper in the DOM
+  // see https://github.com/videojs/video.js/pull/3856
+  render() {
+    return (
+      <div> 
+        <div data-vjs-player>
+          <video ref={ node => this.videoNode = node } className="video-js"></video>
+        </div>
+      </div>
+    )
+  }
+}
+
+
 
 export default App;
